5196ad9e3c335b600df2fbffd9ba0cabba27b661
Ethan Graham <ethangraham43@gmail.com>
2022-10-04 08:18:52 -0400

    w4d2 hw

--- w4d2/skeleton/lib/board.rb
+++ w4d2/skeleton/lib/board.rb
@@ -2,16 +2,43 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14){Array.new}
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    @cups.each_with_index do |cup, i|
+      next if i == 6 || i == 13
+      4.times do 
+        cup << :stone
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise 'Starting cup is empty' if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+    idx = start_pos
+    idx = 0 if idx > 13
+    until stones.empty?
+      idx += 1
+      if idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[idx] << stones.pop
+      end
+    end
+    render
   end
 
   def next_turn(ending_cup_idx)
